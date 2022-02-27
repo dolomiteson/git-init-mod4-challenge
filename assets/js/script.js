2ff3ac542eeecff4b44467d8091638e128c417ac
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-02-27 10:42:59 -0600

    Added intro form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Variable Elements
 var highScoreForm = document.querySelector("#highscores");
+var quizIntro = document.querySelector("#quiz-intro");
 
 /* General Functionality */
 function revealElement(element){
@@ -10,21 +11,18 @@ function hideElement(element){
     element.classList.add("hidden");
 }
 
-/* Highscore Form Functionality */
+/* Form presentation functionality */
+
+// TODO: Need to create unversal way to display and hide form elements based on selection.
 
 // Funtion that will reveal the Highscores element
 function revealHighscores(){
     revealElement(highScoreForm);
+    hideElement(quizIntro);
 }
 
 // Funtion to hide the Highscores element
 function goBack(){
     hideElement(highScoreForm);
+    revealElement(quizIntro);
 }
-
-
-
-// highScoreLink.addEventListener("click", function() {
-//     var element = document.querySelector("#highscores");
-//     element.classList.remove("hidden");
-// });
