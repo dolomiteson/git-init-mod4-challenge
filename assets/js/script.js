f56d8063c6506d204aa28e08d9f9fd2c59c0fbcd
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-02-28 10:44:16 -0600

    Updates to functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,39 @@ formsArray.push(highScoreForm);
 var questionForm = document.querySelector("#question-form");
 formsArray.push(questionForm);
 
+// Test Question
+var question = {
+    ask: "Is this question appearing in the form?",
+    correct:  {
+        actual: "Yes",
+        isTrue: true 
+    },
+    incorrect:  {
+        actual: "No",
+        isTrue: false 
+    }
+};
+
 
 /* General Functionality */
+
+// Funtion that reveals selected element while hiding all other elements in an array
 function revealElement(element, elementArr){
-    var ele = element;
+
     for(i=0; i < elementArr.length; i++){
-        if(ele !== elementArr[i]){
+        if(element !== elementArr[i]){
             elementArr[i].classList.add("hidden");
         }
         else{
-            ele.classList.remove("hidden");
+            element.classList.remove("hidden");
         }
+    }
+}
+
+// Function that will remove all children of the question form
+function removeQuestion(){
+    while (questionForm.firstChild) {
+        questionForm.removeChild(questionForm.firstChild);
     }
 }
 
@@ -32,16 +54,30 @@ function revealElement(element, elementArr){
 // Funtion that will reveal the Highscores element
 function revealHighscores(){
     revealElement(highScoreForm, formsArray);
+    removeQuestion();
 }
 
 // Funtion that will reveal the quiz form and start quiz
 function revealQuiz(){
     revealElement(questionForm, formsArray);
+    genQuestionElements(question);
 }
 
 // Funtion to hide the Highscores element and present Quiz Intro
 function revealQuizIntro(){
     revealElement(quizIntro, formsArray);
+}
+
+//TODO: Create function to generate question
+function genQuestionElements(questionArr){
+
+    // Generate Questions
+    var question = document.createElement("p");
+    question.textContent = questionArr.ask;
+    questionForm.appendChild(question);
+
+    // Add Buttons
+    alert(Object.keys(questionArr).length);
 }
 
 //TODO: Create funtion that will traverse questions to generate on the same form element
